c5d06c0498b62455fceae85f008ff24c3caedac7
Abhinav Tiwari <abhinav2506293@gmail.com>
2017-11-12 12:53:05 +0530

    Protect email-id

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
 
 	$('.collapsible').collapsible();	
 	$(".button-collapse").sideNav();
-	
+})
+
+$(document).ready(function(){
+	var $username = "abhinav2506293";
+	var $hostname = "gmail.com";
+	combine = '<p>' + $username +"@"+ $hostname + '</p>';
+	$('.email-id').html(combine);
 })
