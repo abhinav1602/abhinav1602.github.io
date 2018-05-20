f70f314facbba4f5f232e1132a360717fd8a5623
Abhinav Tiwari <abhinav2506293@gmail.com>
2018-05-20 20:19:10 +0530

    Added new company + changes in skills

--- js/app.js
+++ js/app.js
@@ -9,4 +9,9 @@ $(document).ready(function(){
 	var $hostname = "gmail.com";
 	combine = '<p>' + $username +"@"+ $hostname + '</p>';
 	$('.email-id').html(combine);
+	
+	// add push-l2 to every even jobs class div
+	$( ".jobs:odd" ).addClass('push-l2');
+
+
 })
